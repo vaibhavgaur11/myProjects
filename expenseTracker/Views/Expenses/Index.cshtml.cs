31bd921508de3e60a5a0dab6cf0717a79643e841
vaibhavgaur1206 <vg1548@srmist.edu.in>
2026-01-28 23:19:48 +0530

    employeeAp

---
+++ expenseTracker/Views/Expenses/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExpenseTracker.Views.Expenses
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
